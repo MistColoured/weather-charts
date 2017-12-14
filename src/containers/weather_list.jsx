b186678b11b41cc49ab843528ea708e1c4d59342
Michael Allen <mike@mistColoured.com>
2017-12-14 14:48:27 +0000

    Added Google Maps, end of project

--- src/containers/weather_list.jsx
+++ src/containers/weather_list.jsx
@@ -1,20 +1,25 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), temp => temp - 273);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
+
 
     return (
       <tr key={name}>
-        <td>{name}</td>
-        <td>
-          <Sparklines height={120} width={180} data={temps}>
-            <SparklinesLine color="red" />
-          </Sparklines>
-        </td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
+        <td><Chart data={temps} color="blue" units="K" /></td>
+        <td><Chart data={pressures} color="blue" units="hPa" /></td>
+        <td><Chart data={humidities} color="purple" units="%" /></td>
       </tr>
     );
   }
